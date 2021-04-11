360c034a49831141a95fad762485622293a49203
Iaggelis <john_agelis@hotmail.com>
2021-04-11 15:28:11 +0300

    Follow the fmt style

--- wrappers/bm_fftw.c
+++ wrappers/bm_fftw.c
@@ -14,7 +14,7 @@ Err bm_fftw_alloc_real(Bm *bm)
     double *input_buffer  = fftw_alloc_real(n);
 
     printf("[DBG] creating array of doubles:\n");
-    for(size_t i = 0; i < n; ++i){
+    for(size_t i = 0; i < n; ++i) {
         input_buffer[i] = bm->stack[bm->stack_size - 1  - n + i].as_f64;
         printf("[DBG] %f\n", input_buffer[i]);
     }
@@ -29,7 +29,7 @@ Err bm_fftw_alloc_complex(Bm *bm)
     uint64_t n = 2 * bm->stack[bm->stack_size - 1].as_u64;
     fftw_complex *input_buffer  = fftw_alloc_complex(n);
 
-    for(size_t i = 0; i < n; i+=2){
+    for(size_t i = 0; i < n; i+=2) {
         input_buffer[i][0] = bm->stack[bm->stack_size - 1  - n + i].as_f64;
         input_buffer[i][1] = bm->stack[bm->stack_size - 1  - n + i + 1].as_f64;
         printf("[DBG] %f, %f\n", input_buffer[i][0], input_buffer[i][1]);
@@ -60,7 +60,7 @@ Err bm_fftw_plan_dft_r2c_1d(Bm *bm)
     fftw_execute(p);
 
     printf("[DBG] output complex array\n");
-    for(size_t i = 0; i < fft_out_size; ++i){
+    for(size_t i = 0; i < fft_out_size; ++i) {
         printf("[DBG] %f, %fj\n", output_buffer[i][0], output_buffer[i][1]);
     }
     fftw_destroy_plan(p);
@@ -90,7 +90,7 @@ Err bm_fftw_plan_dft_c2r_1d(Bm *bm)
     fftw_execute(p);
 
     printf("[DBG] But to reality\n");
-    for(size_t i = 0; i < fft_out_size; ++i){
+    for(size_t i = 0; i < fft_out_size; ++i) {
         // FFTW scales the output so we need to divide by the length
         output_buffer[i] /= (double) n;
         printf("[DBG] %f\n", output_buffer[i]);
